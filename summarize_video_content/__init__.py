075df75185730b25ac301086879d3f732911990f
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-03-11 22:17:12 +0000

    removed unused commands from project.scripts in toml

--- summarize_video_content/__init__.py
+++ summarize_video_content/__init__.py
@@ -3,5 +3,6 @@ from summarize_video_content.src import app
 
 from summarize_video_content.src.app import *
 from summarize_video_content.src.message_handler import *
+import summarize_video_content as summarize_video_content
 
-__all__ = [ "app" ]
+__all__ = [ "app", "summarize_video_content" ]
