9321c0e9a3fb560f7d939711caa89458208e5a04
Adrian Wright <wrightadrian@microsoft.com>
2025-04-04 10:27:25 -0400

    fix: --no-emit-workspace on summarize video content

--- export-requirements.sh
+++ export-requirements.sh
@@ -2,4 +2,4 @@
 
 uv export --no-emit-workspace --directory chunk_video_content --output-file requirements.txt
 uv export --no-emit-workspace --directory index_file_api --output-file requirements.txt
-uv export --directory summarize_video_content --output-file requirements.txt
+uv export --no-emit-workspace --directory summarize_video_content --output-file requirements.txt
